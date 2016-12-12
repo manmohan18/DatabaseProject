197954789dd70e3f3b73d1ef5dc3e965b5a9953b
Manmohan Singh <manmohan_1896@hotmail.com>
2016-12-11 22:53:13 -0500

    df

--- DatabaseProject/MasterPage.Master.cs
+++ DatabaseProject/MasterPage.Master.cs
@@ -11,7 +11,21 @@ namespace DatabaseProject
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            string dateDay = DateTime.Now.Date.ToLongDateString().ToString();
+            lblDateDay.Text = dateDay;
 
+
+            if (Application["DefPageCounter"] == null)
+            {
+                Application["DefPageCounter"] = 1;
+            }
+            else
+            {
+                Application.Lock();
+                Application["DefPageCounter"] = (int)Application["DefPageCounter"] + 1;
+                Application.UnLock();
+            }
+            visitCounts.Text = Application["DefPageCounter"].ToString();
         }
     }
 }
