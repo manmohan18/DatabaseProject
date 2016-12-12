35cc280ee1630f1b3dcd213b28d37227367209cb
Manmohan Singh <manmohan_1896@hotmail.com>
2016-12-11 23:38:03 -0500

    ch

--- DatabaseProject/RecipeDetails.aspx.cs
+++ DatabaseProject/RecipeDetails.aspx.cs
@@ -13,6 +13,16 @@ namespace DatabaseProject
 {
     public partial class RecipeDetails : System.Web.UI.Page
     {
+
+        protected void Page_PreInit(object sender, EventArgs e)
+        {
+            HttpCookie themeCookie = Request.Cookies["pageTheme"];
+            if (themeCookie != null)
+            {
+                Page.Theme = themeCookie.Value;
+            }
+        }
+
         protected void Page_Load(object sender, EventArgs e)
         {
             if (!IsPostBack)
